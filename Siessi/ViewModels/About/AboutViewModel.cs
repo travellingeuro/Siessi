5ee75b33f7e546b52533a782fa7e4da5f5003fc5
Pedro Crespo <pedro.crespo.martin@gmail.com>
2021-03-23 12:19:42 -0500

    2303

---
+++ Siessi/ViewModels/About/AboutViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms.Internals;
+
+namespace Siessi.ViewModels.About
+{
+    [Preserve(AllMembers = true)]
+    public class AboutViewModel:BaseViewModel
+    {
+        bool isBusy = false;
+
+        public AboutViewModel()
+        {
+            Title = "Sobre esta _app";
+        }
+
+        
+        public bool IsBusy
+        {
+            get { return isBusy; }
+            set { SetProperty(ref isBusy, value); }
+        }
+
+        public string Content => Resource.Terms;
+    }
+}
---
+++ Siessi/Views/About/AboutPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using Siessi.ViewModels.About;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Internals;
+using Xamarin.Forms.Xaml;
+
+namespace Siessi.Views.About
+{
+    [Preserve(AllMembers = true)]
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AboutPage : ContentPage
+    {
+        AboutViewModel vm;
+
+        public AboutPage()
+        {
+            InitializeComponent();
+            BindingContext = vm = new AboutViewModel();
+        }
+
+        protected override bool OnBackButtonPressed()
+        {
+            if (vm.IsBusy)
+                return false;
+
+            return base.OnBackButtonPressed();
+        }
+    }
+}
